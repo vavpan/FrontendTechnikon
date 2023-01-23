554eee353b8a9775b5c2595e0952a013fe86f9e8
vavpan <v4projectvps@gmail.com>
2023-01-23 10:21:14 +0200

    mandatory fields (login page)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { UserService } from './../../services/user/user-service.service';
 import { AuthenticationService } from 'src/app/services/authentication/AuthenticationService';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,48 +11,54 @@ import { Router } from '@angular/router';
 })
 
 
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 
   loginForm: FormGroup = this.fb.group({
     username: this.fb.control('', Validators.required),
     password: this.fb.control('', Validators.required)
   }
-);
+  );
 
 
   loginUserData = {}
-  constructor(private fb: FormBuilder, private router: Router, private authenticationService: AuthenticationService, private userService: UserService){
+  constructor(private fb: FormBuilder, private router: Router, private authenticationService: AuthenticationService, private userService: UserService) {
 
-  }   
+  }
 
   ngOnInit(): void {
   }
 
 
   onSubmit() {
+
+    if (!this.loginForm.get('username')?.value || !this.loginForm.get('password')?.value) {
+      alert("Please fill in all fields in order to login.");
+      return;
+    }
+
     if (this.loginForm.valid) {
-        let credentials = this.getCredentials();
-        this.authenticationService.login(credentials.username, credentials.password)
+      let credentials = this.getCredentials();
+      this.authenticationService.login(credentials.username, credentials.password)
         .subscribe(response => {
-            if (response) {
-                this.userService.setCurrentUser(credentials.username);
-                this.router.navigate(['home']);
-            } else {
-                alert("Wrong credentials");
-            }
+          if (response) {
+            this.userService.setCurrentUser(credentials.username);
+            this.router.navigate(['home']);
+          } else {
+            alert("Wrong credentials");
+          }
         }, error => {
-            console.log(error);
-            alert("An error occurred, please try again later.");
+          console.log(error);
+          alert("An error occurred, please try again later.");
         });
     }
-}
+  }
 
   private getCredentials(): any {
     return {
       username: this.loginForm.get('username')?.value,
       password: this.loginForm.get('password')?.value
-      
+
     }
   }
- 
+
 }
