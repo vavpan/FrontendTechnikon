9a1f883c52c934a809fe60e309a7b1cd539ad796
vavpan <v4projectvps@gmail.com>
2023-02-15 22:48:09 +0200

    logout bug fixed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { UserService } from './../../services/user/user-service.service';
 import { AuthenticationService } from './../../services/authentication/AuthenticationService';
 import { Component, Input, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +12,15 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   currentUser!: string;
 
-  constructor(private UserService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
-      this.UserService.currentUser.subscribe(user => this.currentUser = user);
+    this.userService.currentUser.subscribe(user => this.currentUser = user);
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd && event.urlAfterRedirects === '/login') {
+        this.currentUser = '';
+      }
+    });
   }
 }
